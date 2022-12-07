2cbe8c25fcef131ec28913b634ce858a4634d1d9
Peu255 <peuwsr@gmail.com>
2022-12-07 20:36:59 -0300

    .

--- js/script.js
+++ js/script.js
@@ -4,9 +4,17 @@ xmlhttp.send();
 xmlDoc = xmlhttp.responseXML;
 
 x = xmlDoc.getElementsByTagName("conteudo");
+y = xmlDoc.getElementsByTagName("blog1");
 
 function OdsTxt() {
 	for (i=x.length-1;i>=0;i--) {
         document.write("<div class='textdiv'><p id='ods13text'>" + x[i].getElementsByTagName("odstext")[0].childNodes[0].nodeValue + "</p></div>");       
     }
 }
+
+function Blog1() {
+    for(i = y.length - 1; i >= 0; i--) {
+        document.write("<div class='div-text-post'><p class='text-post'>" + y[i].getElementsByTagName("text")[0].childNodes[0].nodeValue + "</p></div>");
+    }
+}
+
